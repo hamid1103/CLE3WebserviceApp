a2bd505e9efb96a5fd73e082e905fe158cb15e8c
hamid1103 <35217917+hamid1103@users.noreply.github.com>
2023-03-15 14:08:13 +0100

    PHP Backend done
    V0.1

--- api/Main/index.php
+++ api/Main/index.php
@@ -4,11 +4,46 @@ require_once "includes/actions.php";
 
 //Based on the existence of the GET parameter, 1 of the 2 functions will be called
 //TODO: replace with another method once actions gets too big.
-if (!isset($_GET['id'])) {
-    $data = getRestaurant();
-} else {
-    $data = getRestaurant($_GET['id']);
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    if (isset($_GET['id'])) {
+        $data = getRestaurant($_GET['id']);
+    } elseif (isset($_GET['name'])) {
+        $data = getRestaurantByName(urldecode($_GET['name']));
+    }else{
+        $data = getRestaurant();
+    }
+}elseif ($_SERVER['REQUEST_METHOD'] === 'POST'){
+    if(isset($_POST['action'])){
+        $action = $_POST['action'];
+        //Add Actions Here
+        if($action == 'newKey'){
+            if(!isset($_POST['id']) || !isset($_POST['name']) || !isset($_POST['value'])){
+                $data = 'Missing required header(s). Make sure Request has "id", "name" and "value"';
+            }else{
+                $id = $_POST['id'];
+                $key = $_POST['name'];
+                $value = $_POST['value'];
+                $data = addDataToRestaurant($id, $key, $value);
+            }
+        }elseif ($action == 'addTo'){
+            if(!isset($_POST['id']) || !isset($_POST['name']) || !isset($_POST['value'])){
+                $data = 'Missing required header(s). Make sure Request has "id", "name" and "value"';
+            }else{
+                $id = $_POST['id'];
+                $key = $_POST['name'];
+                $value = $_POST['value'];
+                $data = addDataToKeyInRestaurant($id, $key, $value);
+            }
+        }else{
+            $data = "Error: Unkown Action";
+        }
+    }else{
+        $data = "Error: No Action Defined in post. Make sure you have 'action' header in post with either 'newKey' or 'addTo'";
+    }
 }
+
+
 
 //Get Action(from $_GET or $_POST)
     //Get parameters
