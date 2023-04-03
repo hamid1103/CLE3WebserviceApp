e0cfc69bb5d078367d967fcc281f644a865a0c0e
MossyMoth <thomgroenenberg@gmail.com>
2023-04-03 10:19:26 +0200

    Digital Menu V1.0
    Heeft nog update nodig met:
    Terug knoppen
    Delete previous clear function tegen duplicatie
    Tweede menu API

--- prototypes/Digitaal-Menu/js/main.js
+++ prototypes/Digitaal-Menu/js/main.js
@@ -1,45 +1,55 @@
 window.addEventListener('load', init);
 
-let title;
-let button;
+//global vars
+let apiURL = 'menuDatabase/menu1.json';
+let menu;
+
 
 function init()
 {
-    document.querySelectorAll('table').forEach(item =>{
-        item.style.display = "none";
-    });
-    title = document.getElementsByTagName('H2');
-    document.addEventListener(`click`, binnenMenu);
-
-    button = document.getElementById('terug');
-    button.addEventListener(`click`, terugNaarStart)
+    menu = document.getElementById('menu');
+    collectMenu();
 }
 
 
+function collectMenu() {
+    fetch(apiURL)
+        .then ((response) => {
+            return response.json();
+        })
+        .then(showMenu)
+}
+//Need to add back buttons, stop it from duplicating on multiple clicks, delete other categories when opening one
+function showMenu(resultedMenu) {
+    for (let i = 0; i < resultedMenu.categorie.length; i++) {
+        let categorieDiv = document.createElement('div');
+        let categorieTitel = document.createElement('h1');
 
-function binnenMenu(e) {
-    let target = e.target;
-    if (target.nodeName !== 'H2') {
-        return;
+        categorieDiv.setAttribute('id', Object.getOwnPropertyNames(resultedMenu.categorie[i]));
+        categorieDiv.setAttribute('role', 'button');
+
+        let categorieNaam = Object.getOwnPropertyNames(resultedMenu.categorie[i]);
+        categorieTitel.innerHTML = categorieNaam;
+
+        categorieTitel.addEventListener('click', function (e) {
+                let huidigeCategorie = resultedMenu.categorie[i][categorieNaam];
+                for (let gerechtenLoop = 0; gerechtenLoop < huidigeCategorie.length; gerechtenLoop++) {
+                    let categorieGerecht = document.createElement('div');
+                    let gerechtTitel = document.createElement('h2');
+                    categorieGerecht.appendChild(gerechtTitel);
+                    gerechtTitel.innerHTML = resultedMenu.categorie[i][categorieNaam][gerechtenLoop].naam;
+
+                    gerechtTitel.addEventListener('click', function (e){
+                            let gerechtDetails = document.createElement('h3');
+                            gerechtDetails.innerHTML = `Beschrijving: ${resultedMenu.categorie[i][categorieNaam][gerechtenLoop].beschrijving}<br />Prijs: ${resultedMenu.categorie[i][categorieNaam][gerechtenLoop].prijs}`
+                            categorieGerecht.appendChild(gerechtDetails);
+                    });
+
+                    categorieDiv.appendChild(categorieGerecht);
+                }
+        })
+
+        categorieDiv.appendChild(categorieTitel);
+        menu.appendChild(categorieDiv);
     }
-
-    document.querySelectorAll('div').forEach(item =>{
-        item.style.display = "none";
-    });
-
-    let currentDiv = document.getElementById(target.className);
-    currentDiv.style.display = "block";
-    currentDiv.lastElementChild.style.display = "";
-
-    button.style.display = "block";
-}
-
-function terugNaarStart() {
-    document.querySelectorAll('table').forEach(item =>{
-        item.style.display = "none";
-    });
-    document.querySelectorAll('div').forEach(item =>{
-        item.style.display = "block";
-    });
-    button.style.display = "none";
 }
