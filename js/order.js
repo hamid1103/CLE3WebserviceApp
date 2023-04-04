bb0aa6ede5729e8ed14fe1dbeca300a2bb23da21
Hollaxow <hollaxow@tutanota.com>
2023-04-04 23:33:47 +0200

    added loaded sections from ajax request

--- js/order.js
+++ js/order.js
@@ -11,20 +11,79 @@ let id = params.get('id');
 
 
 async function init() {
-    if (typeof id != null && typeof id == 'number') {
+    if (typeof id != undefined) {
         await fetchRestaurantInfo();
         await fetchRestaurantMenu();
-    }
-    await fetchRestaurants();
-    await loadRestaurants();
+
+        await loadRestaurantInfo();
+        await loadMenu();
+    } //else {
+        await fetchRestaurants();
+        await loadRestaurants();
+    //}
 }
 
 async function loadMenu() {
-    
+    let container = document.getElementById('order-container-items');
+    let nav = document.getElementById('order-category-nav');
+
+    menu.categories.forEach((cat) => {
+        console.log(cat);
+
+        //create the nav
+        let a = document.createElement('a');
+        a.innerText = cat.name;
+        a.setAttribute('href', `#order-category-${cat.id}`);
+        nav.appendChild(a);
+
+        //create the category with dishses
+        //category title
+        h2 = document.createElement('h2');
+        h2.innerText = cat.name;
+        h2.setAttribute('id', `order-category-${cat.id}`);
+
+        //dishes
+        cat.dishes.forEach((dish) => {
+            //section
+            let section = document.createElement('section');
+
+            //name
+            let h3 = document.createElement('h3');
+            h3.innerText = dish.name;
+            //price
+            let price = document.createElement('p');
+            price.innerText = dish.price;
+            //description
+            let descrH = document.createElement('h4');
+            descrH.innerText = 'Beschrijving';
+            let descr = document.createElement('p');
+            descr.innerText = dish.description;
+            //buttone
+            let btn = document.createElement('button');
+            btn.innerText = 'Toevoegen aan bestelling';
+            //TODO add eventhandler for counting total?
+
+            //add to section
+            section.appendChild(h3);
+            section.appendChild(price);
+            section.appendChild(descrH);
+            section.appendChild(descr);
+            section.appendChild(btn);
+
+            //add section to container
+            let container = document.getElementById('order-container-items');
+            container.appendChild(section);
+        });
+    });
+}
+
+async function loadRestaurantInfo() {
+    let title = document.getElementById('order-restaurant-title');
+    title.innerText = restaurant.Name
 }
 
 async function fetchRestaurantMenu() {
-    const res = await fetch(`${URL_MENU}?=id${id}`);
+    const res = await fetch(`${URL_MENUS}?id=${id}`);
     const json = await res.json();
     
     console.log(json);
@@ -33,7 +92,7 @@ async function fetchRestaurantMenu() {
 }
 
 async function fetchRestaurantInfo() {
-    const res = await fetch(`${URL_RESTAURANTS}?=id${id}`);
+    const res = await fetch(`${URL_RESTAURANTS}?id=${id}`);
     const json = await res.json();
     
     console.log(json);
